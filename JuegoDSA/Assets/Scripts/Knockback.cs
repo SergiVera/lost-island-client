275e99d9d54b889d63e0a42e3c5121080f9c18d9
MarcBelmonte <marc.belmonte.santacruz@gmail.com>
2018-12-23 19:44:43 +0100

    Colisiones OK, seguimiento OK

--- JuegoDSA/Assets/Scripts/Knockback.cs
+++ JuegoDSA/Assets/Scripts/Knockback.cs
@@ -15,7 +15,6 @@ public class Knockback : MonoBehaviour {
             Rigidbody2D enemy = other.GetComponent<Rigidbody2D>();
             if(enemy != null)
             {
-                enemy.isKinematic = false;
                 Vector2 difference = enemy.transform.position - transform.position;
                 difference = difference.normalized * thrust;
                 enemy.AddForce(difference, ForceMode2D.Impulse);
@@ -30,7 +29,6 @@ public class Knockback : MonoBehaviour {
         {
             yield return new WaitForSeconds(knockTime);
             enemy.velocity = Vector2.zero;
-            enemy.isKinematic = true;
         }
     }
 }
--- JuegoDSA/Assets/Scripts/Monkey.cs
+++ JuegoDSA/Assets/Scripts/Monkey.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Monkey : Enemy {
+    private Rigidbody2D myRigidbody;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
     public Transform homePosition;
+    public Animator anim;
 
 	// Use this for initialization
 	void Start () {
-         
+        myRigidbody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform; //El transform  es la parte que contiene la info de localizacion (size, rotation, scale, position). Queremos saber donde esta el target
 		
 	}
@@ -23,7 +26,8 @@ public class Monkey : Enemy {
     {
         if(Vector3.Distance(target.position, transform.position)<= chaseRadius && Vector3.Distance(target.position, transform.position)  > attackRadius)
         {
-            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            Vector3 temp = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            myRigidbody.MovePosition(temp);
         }
     }
 }
