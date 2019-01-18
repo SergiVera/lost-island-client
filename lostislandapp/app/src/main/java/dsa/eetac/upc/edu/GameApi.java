1043c66a0c55128197d18ae161926bbb5368c295
CarlosOrbegozo <43339018+CarlosOrbegozo@users.noreply.github.com>
2019-01-18 13:58:56 +0100

    back up. implemented the GameApi interface

--- lostislandapp/app/src/main/java/dsa/eetac/upc/edu/GameApi.java
+++ lostislandapp/app/src/main/java/dsa/eetac/upc/edu/GameApi.java
@@ -2,13 +2,33 @@ package dsa.eetac.upc.edu;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface GameApi {
     String BASE_URL = "http://147.83.7.205:8080/dsaApp/";
 
-    //
+    //auth
+    @POST("auth/login")
+    Call<UserStats> login(User user);
+
+    @POST("auth/deleteaccount")
+    void deleteAccount(User user);
+
+    @PUT("auth/logout")
+    void logout();
+
+    @PUT("auth/newcredentials")
+    Call<Void> newCredentials();
+
+    @POST("auth/auth/sign-up")
+    void signUp(User user);
+
+    //user and objects
     @GET("objects/allobjects")
     Call<List<GameObject>> allObjects();
 
@@ -17,4 +37,13 @@ public interface GameApi {
 
     @GET("users/{idUser}/attributes")
     Call<UserStats> mystats(@Path("idUser") int id_user);
+
+    @GET("users/{idUser}/enemies")
+    Call<List<Enemy>>userEnemies(@Path("idUser") int id_user);
+
+    @POST("users/{idUser}/buyobject/{idGameObject}")
+    void buyObject(@Path("idUser") int id_user, @Path("idGameObject") int id);
+
+    @DELETE("users/{idUser}/sellobject/{idGameObject}")
+    Call<Void> sellObject(@Path("idUser") int id,@Path("idGameObject") int idGameObj );
 }
