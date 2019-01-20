9cf4b5e8349214be0de3aa354f8e02184f0bf4d2
CARLOS <carlos@CARLOS.ORBEGOZO>
2019-01-20 01:15:39 +0100

    backup

--- lostislandapp/app/src/main/java/dsa/eetac/upc/edu/MainActivity.java
+++ lostislandapp/app/src/main/java/dsa/eetac/upc/edu/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -27,22 +28,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private GameApi gameApi;
     private RecyclerView listObjects;
-    private Button objectsBtn;
-    private Button returnObjectsBtn;
-    private Button mystatsBtn;
+    private Button shopBtn;
     private Button scoreboardBtn;
     private Button unityButton;
+
+    private TextView userText;
+    private TextView moneyText;
+    private TextView levelText;
+
+
     ProgressDialog progressDialog;
     AdapterRecycler adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        objectsBtn = findViewById(R.id.objectsBtn);
-        returnObjectsBtn = findViewById(R.id.returnObjects_Btn);
-        scoreboardBtn = findViewById(R.id.scoreboardBtn);
-        mystatsBtn = findViewById(R.id.myStatsBtn);
-        unityButton = findViewById(R.id.launchgamebtn);
+        shopBtn = findViewById(R.id.shop_btn);
+        scoreboardBtn = findViewById(R.id.score_btn);
+
+        userText =  findViewById(R.id.username_txt);
+        moneyText =  findViewById(R.id.money_txt);
+        levelText =  findViewById(R.id.level_txt);
+
+        unityButton = findViewById(R.id.play_btn);
         unityButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
         listObjects.setHasFixedSize(true);
         listObjects.setLayoutManager(new LinearLayoutManager(MainActivity.this));
         createGameApi();
+        myStatsLoad();
         //Progress loading
         progressDialog = new ProgressDialog(this);
         progressDialog.setTitle("Loading...");
@@ -74,40 +83,20 @@ public class MainActivity extends AppCompatActivity {
         gameApi = retrofit.create(GameApi.class);
     }
     public void listObjectsClick(View v){
-        objectsBtn.setVisibility(View.INVISIBLE);
         unityButton.setVisibility(View.INVISIBLE);
         scoreboardBtn.setVisibility(View.INVISIBLE);
-        mystatsBtn.setVisibility(View.INVISIBLE);
-
         listObjects.setVisibility(View.VISIBLE);
-        returnObjectsBtn.setVisibility(View.VISIBLE );
-        gameApi.allObjects().enqueue(objectsCallBack);
+        gameApi.getAllObjects().enqueue(objectsCallBack);
         progressDialog.setTitle("Loading...");
         progressDialog.setMessage("Waiting for the server");
         progressDialog.setCancelable(false);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.show();
 
-    }
-
-    public void returnObjClick(View v){
-        objectsBtn.setVisibility(View.VISIBLE);
-        unityButton.setVisibility(View.VISIBLE);
-        mystatsBtn.setVisibility(View.VISIBLE);
-        scoreboardBtn.setVisibility(View.VISIBLE);
-        listObjects.setVisibility(View.INVISIBLE);
-        returnObjectsBtn.setVisibility(View.INVISIBLE );
     }
 
     public void scoreBoardClick(View v){
-        objectsBtn.setVisibility(View.INVISIBLE);
-        unityButton.setVisibility(View.INVISIBLE);
-        scoreboardBtn.setVisibility(View.INVISIBLE);
-        mystatsBtn.setVisibility(View.INVISIBLE);
-
-        listObjects.setVisibility(View.VISIBLE);
-        returnObjectsBtn.setVisibility(View.VISIBLE );
-        gameApi.allstats().enqueue(statsCallBack);
+        gameApi.allStats().enqueue(statsCallBack);
         progressDialog.setTitle("Loading...");
         progressDialog.setMessage("Waiting for the server");
         progressDialog.setCancelable(false);
@@ -116,15 +105,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void myStatsClick(View v){
-        objectsBtn.setVisibility(View.INVISIBLE);
-        unityButton.setVisibility(View.INVISIBLE);
-        scoreboardBtn.setVisibility(View.INVISIBLE);
-        mystatsBtn.setVisibility(View.INVISIBLE);
-
-        listObjects.setVisibility(View.VISIBLE);
-        returnObjectsBtn.setVisibility(View.VISIBLE );
-        gameApi.mystats(1).enqueue(myStatsCallBack);
+    public void myStatsLoad(){
+        gameApi.userAttributes(1).enqueue(myStatsCallBack);
         progressDialog.setTitle("Loading...");
         progressDialog.setMessage("Waiting for the server");
         progressDialog.setCancelable(false);
@@ -174,6 +156,9 @@ public class MainActivity extends AppCompatActivity {
                 data.addAll(response.body());
                 Collections.sort(data,(d1,d2)->d1.getUsername().compareTo(d2.getUsername()));
                 listObjects.setAdapter(new AdapterRecyclerStats(data));
+                userText.setText("Username: "+data.get(0).getUsername());
+                levelText.setText("Level: "+data.get(0).getLevel());
+                moneyText.setText("Points: "+data.get(0).getPoints());
                 progressDialog.hide();
             } else {
                 Log.d("QuestionsCallback", "Code: " + response.code() + " Message: " + response.message());
